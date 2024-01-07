41f5b24a3af326c5bf02bdc91eb752080357e8d1
Dffernandez <decodevelops@gmail.com>
2024-01-07 17:30:38 -0500

    API futuroSaber

---
+++ API REST/futuroSaber/src/test/java/com/decolution/futuroSaber/ApiFuturoSaberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decolution.futuroSaber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFuturoSaberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
