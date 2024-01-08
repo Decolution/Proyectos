9786dbf06f564f9a1ad8794a99c565abb0957cbe
diego.fernandez <decodevelops@gmail.com>
2024-01-08 12:18:01 -0500

    Metodos obtener studiante y guardarEstudiante

--- API REST/futuroSaber/src/main/java/com/futurodelsaber/futuroSaber/controllers/UserController.java
+++ API REST/futuroSaber/src/main/java/com/futurodelsaber/futuroSaber/controllers/UserController.java
@@ -1,9 +1,33 @@
 package com.futurodelsaber.futuroSaber.controllers;
 
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.futurodelsaber.futuroSaber.models.UserModel;
+import com.futurodelsaber.futuroSaber.services.UserService;
+
 @RestController
+/*En que direccion del servidor PATH se van a activar los metodos de la clase */
+@RequestMapping("/estudiante")
 public class UserController {
-    
+    @Autowired
+    UserService estudianteService;
+
+    @GetMapping()
+    public ArrayList<UserModel> obtenerEstudiantes(){
+        return estudianteService.obtenerEstudiantes();
+    }
+
+    @PostMapping()
+    public UserModel guardarEstudiante(@RequestBody UserModel estudiante){
+        return this.estudianteService.guardarEstudiante(estudiante);
+        
+    }
 }
