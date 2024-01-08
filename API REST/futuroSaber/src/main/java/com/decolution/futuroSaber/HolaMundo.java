b66b6ec176dd145dd3ace3d7f2c28c9961585d59
Dffernandez <decodevelops@gmail.com>
2024-01-07 19:18:36 -0500

    HolaMundo

--- API REST/futuroSaber/src/main/java/com/decolution/futuroSaber/HolaMundo.java
+++ API REST/futuroSaber/src/main/java/com/decolution/futuroSaber/HolaMundo.java
@@ -1,5 +1,13 @@
 package com.decolution.futuroSaber;
 
-public class HolaMundor {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HolaMundo {
+
+    @RequestMapping("/")
+    public String hola(){
+        return "Hola Mundo";
+    }
 }
