019dcfc11bba3ed9d10f869f6a66cd6105f92ccb
diego.fernandez <decodevelops@gmail.com>
2024-01-08 00:03:07 -0500

    Creacion paquetes proyecto

---
+++ API REST/futuroSaber/src/main/java/com/futurodelsaber/futuroSaber/controllers/UserController.java
@@ -0,0 +1,8 @@
+package com.futurodelsaber.futuroSaber.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    
+}
---
+++ API REST/futuroSaber/src/main/java/com/futurodelsaber/futuroSaber/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.futurodelsaber.futuroSaber.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    
+}
